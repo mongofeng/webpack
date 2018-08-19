17a29bc3e3a54c24b25ed40b25b75bfc64d18a6b
yangpu <18312701884@163.com>
2018-08-20 00:50:07 +0800

    修改配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
+  mode: 'production',
   devtool: 'eval-source-map',
   entry: __dirname + '/src/main.js',
 
   output: {
     path: path.resolve(__dirname, 'dist'), // 打包后存放的文件夹
-    filename: 'bundle.js' // 打包后的文件
+    filename: 'app.js' // 打包后的文件
   },
   plugins: [
     new HtmlWebpackPlugin({
