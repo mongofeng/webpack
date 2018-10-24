d2eb5927e09c09347c74d096a22506dd4634636f
yangpu <18312701884@163.com>
2018-10-25 00:15:44 +0800

    添加公共路径

--- PublicPath/src/index.js
+++ PublicPath/src/index.js
@@ -1,4 +1,5 @@
 import './test'
 console.log(process.env.ASSET_PATH)
+__webpack_public_path__ = process.env.ASSET_PATH
 
 console.log(__webpack_public_path__)
--- PublicPath/src/test.js
+++ PublicPath/src/test.js
@@ -1,3 +1,6 @@
+__webpack_public_path__ = '/test'
+
+console.log(__webpack_public_path__)
 module.exports = {
   name: 'test'
 }
